c4af4c7eb810e916799b94dd705faa89b06c2ec9
Megalon <megalongaming@gmail.com>
2020-11-11 14:53:43 -0800

    :crab: Extras is gone :crab:
    Co-authored-by: Jack Baron <jackmbaron@gmail.com>
    Co-authored-by: Brandon Williams <bwei.williams@gmail.com>
    Co-authored-by: DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>

--- wiki/.vuepress/config.js
+++ wiki/.vuepress/config.js
@@ -78,7 +78,6 @@ module.exports = {
           '/modding/': generateSidebar('Modding', [
             '',
             'intro',
-            'extras',
             'linux',
           ]),
           '/models/': generateSidebar('3D Models', [
@@ -119,7 +118,6 @@ module.exports = {
           '/fr/modding/': generateSidebar('Modding', [
             '',
             'intro',
-            'extras',
             'linux',
           ]),
           '/fr/models/': generateSidebar('Modèles 3D', [
