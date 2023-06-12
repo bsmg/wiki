5df646d4a0475a858ec9ca3b44666b5530b63838
Jack Baron <jackmbaron@gmail.com>
2023-06-12 07:51:06 +0100

    configure sidebar

--- wiki/.vitepress/config.ts
+++ wiki/.vitepress/config.ts
@@ -18,16 +18,32 @@ const search = (): DefaultTheme.Config['search'] => {
   }
 }
 
+type Route =
+  | readonly [name: string, path: string, routes?: Route[]]
+  | readonly [name: string, routes: Route[]]
+
 interface SidebarItem {
   name: string
   path: string
-  routes: readonly [name: string, path: string][]
+  routes: Route[]
 }
 
-// TODO: Use
-const sidebar = (
-  ...items: SidebarItem[]
-): Required<DefaultTheme.Config['sidebar']> => {
+const convert = ([text, link, routes]: Route): DefaultTheme.SidebarItem => {
+  if (typeof link === 'string') {
+    return {
+      text,
+      link,
+      items: routes?.map(route => convert(route)),
+    }
+  }
+
+  return {
+    text,
+    items: (link ?? routes)?.map(route => convert(route)),
+  }
+}
+
+const sidebar = (...items: SidebarItem[]): DefaultTheme.SidebarMulti => {
   return Object.fromEntries(
     items.map(({ name, path, routes }) => [
       path,
@@ -35,7 +51,7 @@ const sidebar = (
         {
           text: name,
           link: '.',
-          items: routes.map(([text, link]) => ({ text, link })),
+          items: routes?.map(route => convert(route)),
         },
       ],
     ]),
@@ -61,48 +77,6 @@ export default defineConfig({
           { text: 'Beginners Guide', link: '/beginners-guide.md' },
           { text: 'BSMG Discord', link: 'https://discord.gg/beatsabermods' },
         ],
-
-        outline: [2, 3],
-
-        // sidebar: sidebar(
-        //   {
-        //     name: 'About Us',
-        //     path: '/about/',
-        //     routes: [
-        //       ['Staff', './staff'],
-        //       ['Moderators', './moderators'],
-        //       ['Modders', './modders'],
-        //       ['Supports', './supports'],
-        //       ['Mapping Supports', './mapping-supports'],
-        //       ['3D Artists', './3d-artists'],
-        //       ['Translators', './translators'],
-        //     ],
-        //   },
-        //   {
-        //     name: 'Frequently Asked Questions',
-        //     path: '/faq/',
-        //     routes: [['Install Folder', './install-folder']],
-        //   },
-        //   {
-        //     name: 'Modding',
-        //     path: '/modding/',
-        //     routes: [
-        //       ['.', './intro'],
-        //       ['Linux', './linux'],
-        //     ],
-        //   },
-        //   {
-        //     name: 'Modding',
-        //     path: '/modding/',
-        //     routes: [
-        //       ['', './'],
-        //       ['', './'],
-        //       ['', './'],
-        //       ['', './'],
-        //       ['', './'],
-        //     ],
-        //   },
-        // ),
       },
     },
     fr: {
@@ -157,6 +131,117 @@ export default defineConfig({
 
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    outline: [2, 3],
+
+    sidebar: sidebar(
+      {
+        name: 'About Us',
+        path: '/about/',
+        routes: [
+          ['Staff', './staff'],
+          ['Moderators', './moderators'],
+          ['Modders', './modders'],
+          ['Supports', './supports'],
+          ['Mapping Supports', './mapping-supports'],
+          ['3D Artists', './3d-artists'],
+          ['Translators', './translators'],
+        ],
+      },
+      {
+        name: 'Communities',
+        path: '/communities/',
+        routes: [
+          ['Community Hub', './community-hub'],
+          ['Regional Hub', './regional-hub'],
+        ],
+      },
+      {
+        name: 'Frequently Asked Questions',
+        path: '/faq/',
+        routes: [['Install Folder', './install-folder']],
+      },
+      {
+        name: 'Mapping',
+        path: '/mapping/',
+        routes: [
+          [
+            'Mapping',
+            [
+              ['Basic Mapping', './basic-mapping'],
+              ['Intermediate Mapping', './intermediate-mapping'],
+              ['Extended Mapping', './extended-mapping'],
+            ],
+          ],
+          [
+            'Lighting',
+            [
+              ['Basic Lighting', './basic-lighting'],
+              ['Intermediate Lighting', './intermediate-lighting'],
+              ['Advanced Lighting', './advanced-lighting'],
+              ['Extended Lighting', './extended-lighting'],
+            ],
+          ],
+          [
+            'Audio',
+            [
+              ['Basic Audio Setup', './basic-audio'],
+              ['Advanced Audio Editing', './advanced-audio'],
+            ],
+          ],
+          ['Downmapping', './downmapping'],
+          ['Editor Keybinds', './editor-keybinds'],
+          ['Mapping Glossary', './glossary'],
+          ['How to Testplay', './how-to-testplay'],
+          ['Default Environment Colors', './lighting-defaults'],
+          ['Map Format', './map-format'],
+          ['Coping with Mapping Anxiety', './mapping-anxiety'],
+          ['Mapping Resources Contributors', './mapping-credits'],
+          ['MMA2 User Guide', './mediocre-map-assistant'],
+        ],
+      },
+      {
+        name: 'Modding',
+        path: '/modding/',
+        routes: [
+          ['PC', './pc-mod-dev-intro'],
+          [
+            'Quest',
+            './quest-mod-dev-intro',
+            [
+              ['Config', './quest-mod-dev-config'],
+              ['Custom Types', './quest-mod-dev-custom-types'],
+              ['UI', './quest-mod-dev-ui'],
+            ],
+          ],
+        ],
+      },
+      {
+        name: 'Models',
+        path: '/models/',
+        routes: [
+          ['Custom Sabers', './custom-sabers'],
+          ['Custom Avatars', './custom-avatars'],
+          ['Custom Platforms', './custom-platforms'],
+          ['Custom Notes', './custom-notes'],
+          ['Custom Walls', './custom-walls'],
+          [
+            'Guides',
+            [
+              ['Custom Sabers Guide', './sabers-guide'],
+              ['Custom Avatars Guide', './avatars-guide'],
+              ['Custom Platforms Guide', './platforms-guide'],
+              ['Custom Notes Guide', './notes-guide'],
+              [
+                'Baked Lighting Platforms Guide',
+                './baked-lighting-platforms-guide',
+              ],
+            ],
+          ],
+          ['Shader Migration Guide', './shader-migration'],
+        ],
+      },
+    ),
+
     footer: {
       message: 'Copyright © 2019-2023 Beat Saber Modding Group',
       copyright: 'Licensed under CC BY-NC-SA 4.0',
@@ -168,13 +253,5 @@ export default defineConfig({
     },
 
     search: search(),
-  },
-
-  vite: {
-    resolve: {
-      alias: {
-        '@images': resolve(__dirname, '/.assets/images'),
-      },
-    },
   },
 })
